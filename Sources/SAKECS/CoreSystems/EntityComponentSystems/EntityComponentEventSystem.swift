4e4d59023d40bf61205c5fc2e529e9653b2963ea
Stephen Kac <Stephen@StephenKac.com>
2020-07-15 20:18:49 -0600

    EntityComponentEventSystem has been updated to be cleaner and more efficient.
    There is no longer any optionals.

--- Sources/SAKECS/CoreSystems/EntityComponentSystems/EntityComponentEventSystem.swift
+++ Sources/SAKECS/CoreSystems/EntityComponentSystems/EntityComponentEventSystem.swift
@@ -9,70 +9,104 @@ import Foundation
 import SAKBase
 
 public protocol EntityComponentEventSystem: EntityComponentSystem {
-  // Optional Event handling functions
-  func componentChanged(_ event: (change: ChangeEvent<ComponentFamilyID>, entity: Entity))
-  func tagChanged(_ event: (change: ChangeEvent<Tag>, entity: Entity))
-  func entityChanged(_ event: (change: ChangeType, entity: Entity))
+	// Optional Event handling functions
+	func componentChanged(_ event: (change: ChangeEvent<ComponentFamilyID>,
+																	entity: Entity))
+	func tagChanged(_ event: (change: ChangeEvent<Tag>, entity: Entity))
+	func entityChanged(_ event: (change: ChangeType, entity: Entity))
 }
 
 extension EntityComponentEventSystem {
-  
-  public func componentChanged(_ event: (change: ChangeEvent<ComponentFamilyID>, entity: Entity)) {}
-  public func tagChanged(_ event: (change: ChangeEvent<Tag>, entity: Entity)) {}
-  public func entityChanged(_ event: (change: ChangeType, entity: Entity)) {}
-  
-  //  public func registerForChanges(with handler: @escaping (AnyObject) -> (((ChangeEvent<ComponentFamilyID>, Entity)) -> ()), components: EntityComponent.Type...) -> [Disposable?] {
-  //    var disposables = [Disposable?]()
-  //    for component in components {
-  //      let disposable = ecs?.events.componentEvent.register(for: ChangeEvent.set(component.familyID()), with: self, handler: handler)
-  //      disposables.append(disposable)
-  //    }
-  //    return disposables
-  //  }
-  
-  /// Convenience function for registering for component change events
-  public func registerForChanges(for changes: [ChangeType], of components: EntityComponent.Type...) -> [Disposable?] {
-    var disposables = [Disposable?]()
-    for component in components {
-      if changes.contains(ChangeType.set) {
-        let disposable = ecs?.events.componentEvent.register(for: ChangeEvent.set(component.familyID()), with: self, handler: Self.componentChanged)
-        disposables.append(disposable)
-      }
-      if changes.contains(ChangeType.removed) {
-        let disposable = ecs?.events.componentEvent.register(for: ChangeEvent.removed(component.familyID()), with: self, handler: Self.componentChanged)
-        disposables.append(disposable)
-      }
-    }
-    return disposables
-  }
-  
-  /// Convenience function for registering for tag change events
-  public func registerForChanges(for changes: [ChangeType], of tags: Tag...) -> [Disposable?] {
-    var disposables = [Disposable?]()
-    for tag in tags {
-      if changes.contains(ChangeType.set) {
-        let disposable = ecs?.events.tagEvent.register(for: ChangeEvent.set(tag), with: self, handler: Self.tagChanged)
-        disposables.append(disposable)
-      }
-      if changes.contains(ChangeType.removed) {
-        let disposable = ecs?.events.tagEvent.register(for: ChangeEvent.removed(tag), with: self, handler: Self.tagChanged)
-        disposables.append(disposable)
-      }
-    }
-    return disposables
-  }
-  
-  /// Convenience function for registering for entity change events
-  public func registerForEntityChanges(ofTypes changes: [ChangeType]) -> [Disposable?] {
-    var disposables = [Disposable?]()
-    if changes.contains(ChangeType.set) {
-      let disposable = ecs?.events.entityEvent.register(for: ChangeType.set, with: self, handler: Self.entityChanged)
-      disposables.append(disposable)
-    }
-    if changes.contains(ChangeType.removed) {
-      let disposable = ecs?.events.entityEvent.register(for: ChangeType.removed, with: self, handler: Self.entityChanged)
-      disposables.append(disposable)
-    }
-    return disposables
-  }
+
+	public func componentChanged(_ event: (
+																change: ChangeEvent<ComponentFamilyID>,
+																entity: Entity)) {}
+	public func tagChanged(_ event: (change: ChangeEvent<Tag>, entity: Entity)) {}
+	public func entityChanged(_ event: (change: ChangeType, entity: Entity)) {}
+
+	/// Convenience function for registering for component change events
+	public func registerForChanges(for changes: [ChangeType],
+																 of components: EntityComponent.Type...)
+	-> [DisposeContainer] {
+
+		guard let ecs = ecs else {
+			return []
+		}
+
+		var disposables = [DisposeContainer]()
+
+		for component in components {
+			for change in changes {
+				switch change {
+				case .removed:
+					let disposeContainer = ecs.events.componentEvent.register(
+						for: ChangeEvent.removed(component.familyID()),
+						handler: { self.componentChanged($0) })
+					disposables.append(disposeContainer)
+				case .set:
+					let disposeContainer = ecs.events.componentEvent.register(
+						for: ChangeEvent.set(component.familyID()),
+						handler: { self.componentChanged($0) })
+					disposables.append(disposeContainer)
+				}
+			}
+		}
+
+		return disposables
+	}
+
+	/// Convenience function for registering for tag change events
+	public func registerForChanges(for changes: [ChangeType], of tags: Tag...) -> [DisposeContainer] {
+		guard let ecs = ecs else {
+			return []
+		}
+
+		var disposables = [DisposeContainer]()
+
+		for tag in tags {
+			for change in changes {
+				switch change {
+				case .removed:
+					let disposeContainer = ecs.events.tagEvent.register(
+						for: ChangeEvent.removed(tag),
+						handler: { self.tagChanged($0) })
+					disposables.append(disposeContainer)
+				case .set:
+					let disposeContainer = ecs.events.tagEvent.register(
+						for: ChangeEvent.set(tag),
+						handler: { self.tagChanged($0) })
+					disposables.append(disposeContainer)
+				}
+			}
+		}
+
+		return disposables
+	}
+
+	/// Convenience function for registering for entity change events
+	public func registerForEntityChanges(ofTypes changes: [ChangeType])
+	-> [DisposeContainer] {
+		guard let ecs = ecs else {
+			return []
+		}
+
+		var disposables = [DisposeContainer]()
+
+		for change in changes {
+			switch change {
+			case .removed:
+				let disposeContainer = ecs.events.entityEvent.register(
+					for: ChangeType.removed,
+					handler: { self.entityChanged($0) })
+				disposables.append(disposeContainer)
+			case .set:
+				let disposeContainer = ecs.events.entityEvent.register(
+					for: ChangeType.set,
+					handler: { self.entityChanged($0) })
+				disposables.append(disposeContainer)
+			}
+		}
+
+		return disposables
+	}
 }
--- Sources/SAKECS/CoreSystems/EventSystems/EventSystem.swift
+++ Sources/SAKECS/CoreSystems/EventSystems/EventSystem.swift
@@ -10,46 +10,36 @@ import SAKBase
 
 /// A system that allows events to be registered for and raised for a given type
 public class EventSystem<EventKey: Hashable, EventValue: Any> {
-  public typealias RaisedValue = (EventKey, EventValue)
-  
-  /// All events for the EventKeys
-  internal var events = Dictionary<EventKey, Event<RaisedValue>>()
-  
-  public init() {}
-  
-  /// Registers a class for an event witch is tied to the classes lifespan or can be disposed with the returned Disposable item.
-  public func register<Target>(for event: EventKey, with target: Target, handler: @escaping (Target) -> ((RaisedValue) -> ())) -> Disposable? where Target : AnyObject {
-    if events[event] != nil {
-      return events[event]?.addHandler(target, handler: handler)
-    } else {
-      let newEvent = Event<RaisedValue>()
-      let disp = newEvent.addHandler(target, handler: handler)
-      events[event] = newEvent
-      return disp
-    }
-  }
-  
-  /// Regisers a target to have its handler called when the event is raised. Objects are autmatically deregistered once
-  public func register(for event: EventKey, with target: AnyObject, handler: @escaping ((AnyObject, RaisedValue) -> (Bool))) -> Disposable? {
-    if events[event] != nil {
-      return events[event]?.addHandler(target, handler: handler)
-    } else {
-      let newEvent = Event<RaisedValue>()
-      let disp = newEvent.addHandler(target, handler: handler)
-      events[event] = newEvent
-      return disp
-    }
-  }
-  
-  /// Tells all the listeners for the event
-  public func raise(_ event: EventKey, value: EventValue) {
-    if OperationQueue.current == OperationQueue.main {
-      events[event]?.raise((event, value))
-    } else {
-      OperationQueue.main.addOperation { [weak self] in
-        self?.events[event]?.raise((event, value))
-      }
-    }
-  }
+	public typealias RaisedValue = (EventKey, EventValue)
+	public typealias Handler = (RaisedValue) -> Void
+
+	/// All events for the EventKeys
+	internal var events = Dictionary<EventKey, Event<RaisedValue>>()
+
+	public init() {}
+
+	/// Registers a class for an event witch is tied to the classes lifespan by the DisposeContainer.
+	public func register(for event: EventKey,
+											 handler: @escaping Handler) -> DisposeContainer {
+		if let event = events[event] {
+			return event.addHandler(handler: handler)
+		} else {
+			let newEvent = Event<RaisedValue>()
+			let disp = newEvent.addHandler(handler: handler)
+			events[event] = newEvent
+			return disp
+		}
+	}
+
+	/// Raises the event for all listeners
+	public func raise(_ event: EventKey, value: EventValue) {
+		if OperationQueue.current == OperationQueue.main {
+			events[event]?.raise((event, value))
+		} else {
+			OperationQueue.main.addOperation { [weak self] in
+				self?.events[event]?.raise((event, value))
+			}
+		}
+	}
 }
 
